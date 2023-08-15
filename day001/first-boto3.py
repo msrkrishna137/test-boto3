669e611e02742c0810bc86d0510017a2c5254889
msrkrishna137 <msrkrishna137@gmail.com>
2023-08-15 21:15:43 +0530

    update script

--- day001/first-boto3.py
+++ day001/first-boto3.py
@@ -2,7 +2,7 @@
 
 import boto3 
 aws_man_con=boto3.session.Session(profile_name="default")
-iam_con=aws_man_con.resourse('iam')
+iam_con=aws_man_con.resource('iam')
 
 for each_user in iam_con.users.all():
     print(each_user.name)
